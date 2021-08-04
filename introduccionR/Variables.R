b6e1ea911fd545e158d81db1adcd9179307e02b2
katherineggs <katherinegarcia@ufm.edu>
2021-08-04 14:23:02 -0600

    Trabajos semana 1

---
+++ introduccionR/Variables.R
@@ -0,0 +1,88 @@
+## Instalar librerias
+
+install.packages("dplyr")
+install.packages("RMySQL")
+install.packages("lubridate")
+install.packages("openxlsx")
+install.packages("stringr")
+install.packages("readr")
+
+install.packages("tidyverse")
+
+## cargar librerias
+
+library(dplyr)
+require(dplyr)
+
+## folder / location
+getwd()
+setwd("../Documents/")
+
+## Data types and Structures in R
+
+### String - Character
+string <- "this is a string"
+class(string) # clase de la variable
+typeof(string)# tipo de la variable
+length(string)# elementos en la variable -> 1
+nchar(string) # numero de caracteres y espacios -> 16 
+
+
+### Double
+number <- 23334
+class(number)
+typeof(number)
+length(number)
+
+### Integers
+integer <- 3L
+class(integer)
+typeof(integer)
+
+### Logical
+logical <- FALSE # tambien puede ser F
+class(logical)
+typeof(logical)
+
+### Vector
+vector <- c("hola", "a", "todos")
+length(vector)
+vector[2] # lo que este en esta posicion
+
+### Factores
+vector2 <- c("hola", "a", "todos", "como", "estan", "hola", "a", "todos", "como", "estan", "hola", "a",)
+factor <- factor(vector2)
+
+### Factores Ordenados
+factor2 <- ordered(factor, levels = c("hola", "a", "todos", "como", "estan"))
+
+
+### Data Frames
+df <- data.frame(
+  c1 = c("this", "is", "a", "vector", "of", "things"),
+  c2 = 1:6,
+  c3 = letters[1:6],
+  stringsAsFactors = FALSE
+)
+
+View(df) # para visualizar
+
+dfCopy <- data.frame(
+  c1 = c("this", "is", "a", "vector", "of", "things"),
+  c2 = 1:6,
+  c3 = letters[1:6],
+)
+
+str(df)
+
+names(df) # para ver nombres de las columnas
+names(df) <- c("Columna 1", "Columna 2", "Columna 3") # para hacer overwrite pero debe ser de la misma longitud
+names(df)
+
+colnames(df)
+head(df, 3)
+tail(df, 1)
+nrow(df)
+ncol(df)
+
+
