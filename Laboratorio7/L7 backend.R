1ed7d31e68622c96527435d11da045d5aa0ed257
katherineggs <katherinegarcia@ufm.edu>
2021-10-13 03:04:16 -0600

    lab7

--- Laboratorio7/L7 backend.R
+++ Laboratorio7/L7 backend.R
@@ -1,5 +1,241 @@
+library(dplyr)
+library(lubridate)
 
 # leer csv
 data <- read.csv("c1.csv")
 rename(data, ID = `idPoste`)
 View(data)
+
+data <- data %>%
+  mutate(factura = gsub("^ Q-   ", "",factura)) %>% mutate(factura = gsub("^ Q", "",factura)) %>%
+  
+  mutate(Camion_5 = gsub("^ Q-   ", "",Camion_5)) %>% mutate(Camion_5 = gsub("^ Q", "",Camion_5)) %>%
+  mutate(fijoCamion_5 = gsub("^ Q-   ", "",fijoCamion_5)) %>% mutate(fijoCamion_5 = gsub("^ Q", "",fijoCamion_5)) %>%
+  mutate(directoCamion_5 = gsub("^ Q-   ", "",directoCamion_5)) %>% mutate(directoCamion_5 = gsub("^ Q", "",directoCamion_5)) %>%
+  mutate(Pickup = gsub("^ Q-   ", "",Pickup)) %>% mutate(Pickup = gsub("^ Q", "",Pickup)) %>%
+  mutate(fijoPickup = gsub("^ Q-   ", "",fijoPickup)) %>% mutate(fijoPickup = gsub("^ Q", "",fijoPickup)) %>%
+  mutate(directoPickup = gsub("^ Q-   ", "",directoPickup)) %>% mutate(directoPickup = gsub("^ Q", "",directoPickup)) %>%
+  mutate(Moto = gsub("^ Q-   ", "",Moto)) %>% mutate(Moto = gsub("^ Q", "",Moto)) %>%
+  mutate(fijoMoto = gsub("^ Q-   ", "",fijoMoto)) %>% mutate(fijoMoto = gsub("^ Q", "",fijoMoto)) %>%
+  mutate(directoMoto = gsub("^ Q-   ", "",directoMoto)) %>% mutate(directoMoto = gsub("^ Q", "",directoMoto)) %>%
+  
+  mutate(Camion_5 = as.integer(Camion_5)) %>% mutate(Pickup = as.numeric(Pickup)) %>% mutate(Moto = as.numeric(Moto)) %>%
+  mutate(fijoCamion_5 = as.numeric(fijoCamion_5)) %>% mutate(fijoPickup = as.numeric(fijoPickup)) %>%
+  mutate(fijoMoto = as.numeric(fijoMoto)) %>%
+  
+  mutate(directoCamion_5 = as.numeric(directoCamion_5)) %>%
+  mutate(directoPickup = as.numeric(directoPickup)) %>%
+  mutate(directoMoto = as.numeric(directoMoto)) %>%
+  mutate(factura = as.integer(factura)) %>%
+  
+  mutate(Fecha = sub("(.{6})(.*)", "\\120\\2", Fecha)) %>%
+  mutate(Fecha = as_date(Fecha, format = "%d-%m-%Y")) %>%
+  
+  replace(is.na(.), 0) %>%
+  mutate(tipoVehiculo = ifelse((Camion_5 != 0),"Camion",
+                               ifelse((Pickup != 0),"Pickup",
+                                      ifelse((Moto != 0),"Moto","")))) %>%
+  mutate(distancia = ifelse(X5.30 == " x ", "5-30",
+                            ifelse(X30.45 == " x ", "30-45",
+                                   ifelse(X45.75 == " x ", "45-75",
+                                          ifelse(X75.120 == " x ", "75-120",
+                                                 ifelse(X120. == " x ", "120+", ""))))))
+
+data$X <- NULL
+data$X.1 <- NULL
+data$X.2 <- NULL
+data$X.3 <- NULL
+data$X.4 <- NULL
+data$X.5 <- NULL  
+
+# ●	Estado de resultados breve del 2017.
+ingresos <- data %>%
+  select(factura) %>%
+  summarise(ingresos = sum(factura))
+  #select(Camion_5, Pickup, Moto) %>%
+  #summarise(ingresos = sum(Camion_5)+sum(Pickup)+sum(Moto))
+
+cstDir <- data %>%
+  select(directoCamion_5, directoPickup, directoMoto) %>%
+  summarise(cst = sum(directoCamion_5)+sum(directoPickup)+sum(directoMoto))
+
+cstFijos <- data %>%
+  select(fijoCamion_5, fijoPickup, fijoMoto) %>%
+  summarise(cst = sum(fijoCamion_5)+sum(fijoPickup)+sum(fijoMoto))
+
+impts <- data %>%
+  select(Camion_5, Pickup, Moto, factura) %>%
+  summarise(ingresos = sum(Camion_5)+sum(Pickup)+sum(Moto), fact = sum(factura))%>%
+  summarise(impt = fact - ingresos) %>%
+  summarise(impt)
+
+data %>%
+  select(Cod,origen) %>%
+  group_by(Cod) %>%
+  summarise(cant = n()) %>%
+  arrange(desc(cant)) %>%
+  View()
+
+data %>%
+  select(Cod,origen,Fecha,factura) %>%
+  group_by(Cod, month(Fecha)) %>%
+  filter(Cod == "REVISION") %>%
+  summarise(money = sum(factura)) %>%
+  summarise(mean(money)) %>%
+  #arrange(desc(cant)) %>%
+  View()
+
+data %>%
+  select(Fecha) %>%
+  group_by(year(Fecha)) %>%
+  summarise(n()) %>%
+  View()
+
+# vehiculo con mas entradas
+data %>%
+  select(Camion_5, Pickup, Moto, origen, factura) %>%
+  group_by(origen, tipoVehiculo) %>%
+  filter(tipoVehiculo == "Pickup") %>%
+  summarise(Q = sum(factura)) %>%
+  summarise(mean(Q))%>%
+  #summarise(camion = sum(Camion_5), pick = sum(Pickup), mot = sum(Moto)) %>%
+  View()
+
+data %>%
+  select(tipoVehiculo,distancia) %>%
+  group_by(tipoVehiculo, distancia) %>%
+  summarise(n = n()) %>%
+  arrange(desc(n)) %>%
+  View()
+
+data %>%
+  select(tipoVehiculo, Cod) %>%
+  group_by(tipoVehiculo, Cod) %>%
+  summarise(n = n()) %>%
+  #summarise(mean(Q))%>%
+  #summarise(camion = sum(Camion_5), pick = sum(Pickup), mot = sum(Moto)) %>%
+  arrange(desc(n)) %>%
+  View()
+
+# vehiculo con menos distancias
+data %>%
+  select(Cod, tipoVehiculo, distancia) %>%
+  group_by(tipoVehiculo, distancia) %>%
+  #filter(tipoVehiculo == "Camion") %>%
+  summarise(n = n()) %>%
+  arrange(desc(n)) %>%
+  View()
+
+View(data)
+
+# estado de resultados 2017
+ventas <- data %>%
+  select(factura, Pickup, Camion_5, Moto) %>%
+  summarise(ventas = sum(factura))
+
+costoVenta <- data %>%
+  select(directoCamion_5, directoMoto, directoPickup, fijoCamion_5, fijoPickup, fijoMoto) %>%
+  summarise(ctV = sum(fijoCamion_5) + sum(fijoPickup) + sum(fijoMoto)
+            +sum(directoCamion_5) + sum(directoPickup) + sum(directoMoto))
+
+ut <- ventas - costoVenta
+ut # EN QUETZALEEEES
+
+# 2018
+ventas18 <- data %>%
+  select(factura, Fecha) %>%
+  filter(month(Fecha) <= 9) %>%
+  #group_by(month(Fecha)) %>%
+  summarise(ventas = sum(factura)*0.75)
+
+View(ventas18)
+
+costoVenta18 <- data %>%
+  select(Camion_5, Moto, Pickup, Fecha) %>%
+  #select(directoCamion_5, directoMoto, directoPickup, fijoCamion_5, fijoPickup, fijoMoto, Fecha) %>%
+  filter(month(Fecha) <= 9) %>%
+  #group_by(month(Fecha)) %>%
+  summarise(ctV = (sum(Camion_5) + sum(Pickup) + sum(Moto))*0.97)
+  #summarise(ctV = sum(fijoCamion_5) + sum(fijoPickup) + sum(fijoMoto)
+            #+sum(directoCamion_5) + sum(directoPickup) + sum(directoMoto))
+
+costoVenta18
+
+ut18 <- ventas18 - costoVenta18
+ut18 # EN QUETZALEEEES
+
+utDeseada19 <- ut18 * 1.1
+utDeseada19
+
+# Tarifario por servicio
+factTVehiculo <- data %>%
+  select(tipoVehiculo, factura, Cod) %>%
+  group_by(Cod) %>%
+  summarise(servicios = sum(factura), cant = n()) %>%
+  summarise(precioUnidad = servicios/cant, Cod) %>%
+  View()
+
+
+View(factTVehiculo)
+
+# distancias
+data %>%
+  select(origen, distancia) %>%
+  group_by(origen, distancia) %>%
+  summarise(n = n()) %>%
+  arrange(desc(n)) %>%
+  View()
+
+# costos por mes
+data %>%
+  select(Camion_5, Moto, Pickup, Fecha) %>%
+  group_by(month(Fecha)) %>%
+  summarise(costos = sum(Camion_5) + sum(Pickup) + sum(Moto)) %>%
+  summarise(fluct = 100 - ((costos*100)/2345269)) %>%
+  summarise(sum(fluct)) %>%
+  View()
+
+# Perdidas
+#  Costo max y fact min (por codgo) resto fact – costo  cuanto le npodemos perder 
+data %>%
+  select(Camion_5, Moto, Pickup, factura, Cod) %>%
+  group_by(Cod) %>%
+  mutate(costo = ifelse(Camion_5 != 0, Camion_5, ifelse(Pickup != 0, Pickup, ifelse(Moto != 0, Moto, 0)))) %>%
+  summarise(costoMax = max(costo), factMin = min((factura)) ) %>%
+  summarise(Cod, perdida = factMin - costoMax) %>%
+  View()
+  
+# abrir mas centros 
+data %>%
+  select(distancia) %>%
+  group_by(distancia) %>%
+  summarise(n = n()) %>%
+  arrange(desc(n)) %>%
+  View()
+
+# 80 -20
+ventasTotales <- sum(data$factura)
+data %>%
+  select(Cod, factura) %>%
+  group_by(Cod) %>%
+  summarise(ingresoServicio = sum(factura)) %>%
+  summarise(`%Utilidad` = (ingresoServicio * 100)/ventasTotales, Cod) %>%
+  arrange(desc(`%Utilidad`)) %>%
+  View()
+
+# recorridos mas efectivos
+data %>%
+  select(distancia, Cod) %>%
+  group_by(distancia, Cod) %>%
+  filter(distancia == "5-30" || distancia == "30-45") %>%
+  summarise(n = n()) %>%
+  arrange(desc(n)) %>%
+  View()
+
+# 10%
+margenGanancia <- (ut / ventas) * 100
+margenGanancia
+
+margenPrecio <- 
+
+
--- Laboratorio7/lab7.md
+++ Laboratorio7/lab7.md
@@ -7,5 +7,184 @@ Katherine Garcia - 20190418
 -   Utilidad superior a US$1 Millón. - 2017
 -   Han tenido una baja del 25% con respecto al año anterior. Septiembre
     2018
--   Ofrecimos un estudio del mercado, para que el siguiente año la
-    empresa pueda crecer el 10% con respecto al 2018. - 2019
+-   Estudio del mercado, para que el siguiente año la empresa pueda
+    crecer el 10% con respecto al 2018. - 2019
+-   Entender cómo funcionó el 2017.
+
+## Existen 4 tiendas que cubren los 10 servicios que ofrecen
+
+``` r
+data %>%
+  select(Cod,origen) %>%
+  group_by(tiendaNo. =origen) %>%
+  summarise(cantServicios = n_distinct(Cod))
+```
+
+    ## # A tibble: 4 × 2
+    ##   tiendaNo. cantServicios
+    ##       <int>         <int>
+    ## 1    150224            10
+    ## 2    150277            10
+    ## 3    150278            10
+    ## 4    150841            10
+
+## Estado de Resultados - 2017 en Quetzales
+
+``` r
+data %>%
+  select(factura, fijoPickup, fijoCamion_5, fijoMoto, directoPickup, directoCamion_5, directoMoto) %>%
+  summarise(ventas = sum(factura), ctV = sum(fijoCamion_5) + sum(fijoPickup) + sum(fijoMoto)
+            + sum(directoCamion_5) + sum(directoPickup) + sum(directoMoto)) %>%
+  summarise(Utilidad = ventas - ctV) # EN QUETZALES
+```
+
+    ##   Utilidad
+    ## 1  8383552
+
+## Como fue el 2018 en Quetzales
+
+``` r
+data %>%
+  select(Fecha, factura, fijoPickup, fijoCamion_5, fijoMoto, directoPickup, directoCamion_5, directoMoto) %>%
+  filter(month(Fecha) <= 9) %>%
+  summarise(ventas = sum(factura) * 0.75, ctV = (sum(fijoCamion_5) + sum(fijoPickup) + sum(fijoMoto)
+            + sum(directoCamion_5) + sum(directoPickup) + sum(directoMoto)) *0.97) %>%
+  summarise(Utilidad = ventas - ctV) # EN QUETZALES
+```
+
+    ##   Utilidad
+    ## 1 66769.18
+
+## Tarifario por servicio
+
+``` r
+ data %>%
+  select(tipoVehiculo, factura, Cod) %>%
+  group_by(Servicio = Cod) %>%
+  summarise(servicios = sum(factura), cant = n()) %>%
+  summarise(precioUnidad = servicios/cant, Servicio) 
+```
+
+    ## # A tibble: 10 × 2
+    ##    precioUnidad Servicio                
+    ##           <dbl> <chr>                   
+    ##  1         124. CAMBIO_CORRECTIVO       
+    ##  2         153. CAMBIO_FUSIBLE          
+    ##  3         176. CAMBIO_PUENTES          
+    ##  4         176. OTRO                    
+    ##  5         132. REVISION                
+    ##  6         157. REVISION_TRANSFORMADOR  
+    ##  7         141. VERIFICACION_INDICADORES
+    ##  8         131. VERIFICACION_MEDIDORES  
+    ##  9         128. VISITA                  
+    ## 10         176. VISITA_POR_CORRECCION
+
+## ¿Cuándo podríamos perderle a un mantenimiento y/o reparación?
+
+``` r
+data %>%
+  select(Camion_5, Moto, Pickup, factura, Cod) %>%
+  group_by(Servicio = Cod) %>%
+  mutate(costo = ifelse(Camion_5 != 0, Camion_5, ifelse(Pickup != 0, Pickup, ifelse(Moto != 0, Moto, 0)))) %>%
+  summarise(costoMax = max(costo), factMin = min((factura)), factMax = max(factura)) %>%
+  summarise(Servicio, Perdida = (factMin - costoMax))
+```
+
+    ## # A tibble: 10 × 2
+    ##    Servicio                 Perdida
+    ##    <chr>                      <dbl>
+    ##  1 CAMBIO_CORRECTIVO           -401
+    ##  2 CAMBIO_FUSIBLE              -446
+    ##  3 CAMBIO_PUENTES              -363
+    ##  4 OTRO                        -420
+    ##  5 REVISION                    -420
+    ##  6 REVISION_TRANSFORMADOR      -451
+    ##  7 VERIFICACION_INDICADORES    -447
+    ##  8 VERIFICACION_MEDIDORES      -381
+    ##  9 VISITA                      -387
+    ## 10 VISITA_POR_CORRECCION       -409
+
+## Abrir más centros?
+
+``` r
+data %>%
+  select(distancia) %>%
+  group_by(distancia) %>%
+  summarise(cantViajes = n()) %>%
+  arrange(desc(cantViajes))
+```
+
+    ## # A tibble: 5 × 2
+    ##   distancia cantViajes
+    ##   <chr>          <int>
+    ## 1 75-120        110764
+    ## 2 30-45          52745
+    ## 3 5-30           39559
+    ## 4 45-75          34284
+    ## 5 120+           26373
+
+## 80 - 20 de lo facturado
+
+``` r
+ventasTotales <- sum(data$factura)
+data %>%
+  select(Cod, factura) %>%
+  group_by(Cod) %>%
+  summarise(ingresoServicio = sum(factura)) %>%
+  summarise(`%Utilidad` = (ingresoServicio * 100)/ventasTotales, Cod) %>%
+  arrange(desc(`%Utilidad`))
+```
+
+    ## # A tibble: 10 × 2
+    ##    `%Utilidad` Cod                     
+    ##          <dbl> <chr>                   
+    ##  1      32.6   REVISION                
+    ##  2      17.0   VERIFICACION_MEDIDORES  
+    ##  3      12.3   VERIFICACION_INDICADORES
+    ##  4      12.2   CAMBIO_CORRECTIVO       
+    ##  5       8.01  CAMBIO_FUSIBLE          
+    ##  6       7.94  VISITA_POR_CORRECCION   
+    ##  7       5.37  REVISION_TRANSFORMADOR  
+    ##  8       2.83  OTRO                    
+    ##  9       0.887 CAMBIO_PUENTES          
+    ## 10       0.829 VISITA
+
+## Recorridos mas efectivos
+
+``` r
+data %>%
+  select(distancia, Cod) %>%
+  group_by(distancia, Cod) %>%
+  filter(distancia == "5-30" || distancia == "30-45") %>%
+  summarise(n = n()) %>%
+  arrange(desc(n))
+```
+
+    ## `summarise()` has grouped output by 'distancia'. You can override using the `.groups` argument.
+
+    ## # A tibble: 20 × 3
+    ## # Groups:   distancia [2]
+    ##    distancia Cod                          n
+    ##    <chr>     <chr>                    <int>
+    ##  1 30-45     REVISION                 18901
+    ##  2 5-30      REVISION                 14362
+    ##  3 30-45     VERIFICACION_MEDIDORES    9784
+    ##  4 30-45     CAMBIO_CORRECTIVO         8242
+    ##  5 5-30      VERIFICACION_MEDIDORES    7451
+    ##  6 30-45     VERIFICACION_INDICADORES  6389
+    ##  7 5-30      CAMBIO_CORRECTIVO         6315
+    ##  8 5-30      VERIFICACION_INDICADORES  4683
+    ##  9 30-45     CAMBIO_FUSIBLE            3694
+    ## 10 5-30      CAMBIO_FUSIBLE            3341
+    ## 11 30-45     VISITA_POR_CORRECCION     2107
+    ## 12 30-45     REVISION_TRANSFORMADOR    2035
+    ## 13 5-30      REVISION_TRANSFORMADOR    1373
+    ## 14 5-30      VISITA_POR_CORRECCION     1182
+    ## 15 30-45     OTRO                       821
+    ## 16 30-45     VISITA                     500
+    ## 17 5-30      VISITA                     413
+    ## 18 5-30      OTRO                       295
+    ## 19 30-45     CAMBIO_PUENTES             272
+    ## 20 5-30      CAMBIO_PUENTES             144
+
+# Incremento del 10%
